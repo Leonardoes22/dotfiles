ff1c029a606db9c63e88d55db60314c83a8b9190
Leonardo Duarte <leonardoduartevargas@gmail.com>
2024-04-24 10:41:34 -0300

    add zoxide and fzf to install

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# zoxide
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+# obs.: need to add `eval "$(zoxide init zsh)"` to .zshrc
+
+# fzf
+sudo apt install fzf
 
 # Brave Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
