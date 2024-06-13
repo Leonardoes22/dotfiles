cba2ba95107578d3213e6394ab486ab57103511b
Leonardo Duarte <leonardoduartevargas@gmail.com>
2024-06-13 18:42:11 -0300

    add guake

--- install.sh
+++ install.sh
@@ -26,6 +26,10 @@ sudo apt install ripgrep
 sudo apt install fdfind
 # create link to point fd to fdfind
 
+# guake
+sudo apt install guake
+## guake --restore-preferences cfg_name
+
 # Brave Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
