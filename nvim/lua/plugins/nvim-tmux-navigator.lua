2ea5876466d9d613465b8f045723350eb242f544
Leonardo Duarte <leonardoduartevargas@gmail.com>
2025-04-09 16:25:52 +0200

    [nvim] misc changes

--- nvim/lua/plugins/nvim-tmux-navigator.lua
+++ nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,7 +1,7 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  vim.keymap.set("n", "C-h", ":TmuxNavigateLeft<CR>"),
-  vim.keymap.set("n", "C-j", ":TmuxNavigateDown<CR>"),
-  vim.keymap.set("n", "C-k", ":TmuxNavigateUp<CR>"),
-  vim.keymap.set("n", "C-l", ":TmuxNavigateRight<CR>"),
+	"christoomey/vim-tmux-navigator",
+	vim.keymap.set("n", "C-h", "<Cmd>TmuxNavigateLeft<CR>"),
+	vim.keymap.set("n", "C-j", "<Cmd>TmuxNavigateDown<CR>"),
+	vim.keymap.set("n", "C-k", "<Cmd>TmuxNavigateUp<CR>"),
+	vim.keymap.set("n", "C-l", "<Cmd>TmuxNavigateRight<CR>"),
 }
